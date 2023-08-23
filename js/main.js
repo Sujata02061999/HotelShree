e731abe6aad4262d4344da678c979e8249349dbb
GaikwadSujata <jfsjdvspune01.23.sujata.gaikwad@gmail.com>
2023-08-23 20:27:32 +0530

    Banner of Service and javascript of service section is completed

--- js/main.js
+++ js/main.js
@@ -31,3 +31,25 @@ const swiper = new Swiper('.mySwiper', {
     delay:3500,
    },
   });
+//   counter
+document.addEventListener("DOMContentLoaded", () =>{
+    function counter(id, start,end, duration){
+        let obj = document.getElementById(id),
+        current = start,
+        range = end- start,
+        increment = end > start ? 1 : -1;
+        step = Math.abs(Math.floor(duration/range));
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        } , step);
+
+    }
+    counter("count1" , 0, 786,3000);
+    counter("count2" , 100, 1777,2500);
+    counter("count3" , 0, 27,3000);
+    counter("count4" , 0, 3786,3000);
+})
