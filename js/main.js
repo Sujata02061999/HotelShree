e5eb6dbc3bd4e12cfe4dcb12dc74a7001c5a0f08
GaikwadSujata <jfsjdvspune01.23.sujata.gaikwad@gmail.com>
2023-08-30 21:40:23 +0530

    completed Navbar fix using javascript

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// Header Scroll
-// let nav = document.querySelector(`.navbar`);
-// window.onscroll = function(){
-//     if(document.documentElement.scrollTop>50){
-//         nav.classList.add("header-scrolled");
-//     }else{
-//         nav.classList.remove("header-scrolled");
-//     }
-// }
+//Header Scroll
+let nav = document.querySelector(`.navbar`);
+window.onscroll = function(){
+    if(document.documentElement.scrollTop>50){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+}
 
 // Nav Hide
 let navBar = document.querySelectorAll(".nav-link");
